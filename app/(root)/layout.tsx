53ef145421774164ddfd1f821ed5b44f7c68f199
Tanmay Tiwari <tanmaytiwari200905@gmail.com>
2025-04-14 22:33:44 +0530

    Working on Home Page

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,9 +1,19 @@
-import React, { ReactNode } from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import React, { ReactNode } from "react";
 
 const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
-    <div>{children}</div>
-  )
-}
+    <div className="root-layout">
+      <nav>
+        <Link href="/" className="flex items-center gap-2">
+          <Image src="/logo.svg" alt="Logo" width={38} height={32} />
+          <h2 className="text-primary-100">Samyoj</h2>
+        </Link>
+      </nav>
+      {children}
+    </div>
+  );
+};
 
-export default RootLayout
+export default RootLayout;
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
-  )
-}
+    <>
+      <section className="card-cta">
+        <div className="flex flex-col gap-6 max-w-lg">
+          <h2>Master Every Interview with AI-Powered Prep & Feedback</h2>
+          <p className="text-lg">
+            Boost Your Confidence with Real Questions and Real-Time Insights
+          </p>
 
-export default HomePage
+          <Button asChild className="btn-primary max-sm:w-full">
+            <Link href="/interview">Start an Interview</Link>
+          </Button>
+        </div>
+
+        <Image src="/robot.png" alt="robo boii" width={400} height={400} className="max-sm:hidden"/>
+      </section>
+
+      <section className="flex flex-col gap-6 mt-8">
+        <h2>Your Interviews</h2>
+        <div className="interviews-section">
+          <p>You haven&apos;t taken any interviews yet</p>
+        </div>
+      </section>
+
+      <section className="flex flex-col gap-6 mt-8">
+
+      </section>
+    </>
+  );
+};
+
+export default HomePage;
